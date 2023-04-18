17d03ad321eaae733265544ebb63649a6b36f289
masmuhamadjamaludin <masmuhamadjamaludin1@gmail.com>
2023-04-18 10:17:47 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+from http import client
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -26,10 +28,13 @@ def movie_post():
     url_receive = request.form['url_give']
     star_receive = request.form['star_give']
     comment_receive = request.form['comment_give']
+
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive,headers=headers)
+    data = requests.get(url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
+
+    # From here on, we will write the code for extracting data from meta tags
 
     og_image = soup.select_one('meta[property="og:image"]')
     og_title = soup.select_one('meta[property="og:title"]')
@@ -46,12 +51,14 @@ def movie_post():
         'comment': comment_receive,
     }
     db.movies.insert_one(doc)
-    return jsonify({'msg':'POST request!'})
+    return jsonify({'msg': 'POST request!'})
+
 
 @app.route("/movie", methods=["GET"])
 def movie_get():
     movie_list = list(db.movies.find({}, {'_id': False}))
     return jsonify({'movies': movie_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
